7608c5d325287506df8a4094322485bc44a3a3ca
aostagirl <129788679+aostagirl@users.noreply.github.com>
2024-04-11 17:53:26 -0400

    Update run-unit-test.js

--- run-unit-test.js
+++ run-unit-test.js
@@ -133,7 +133,7 @@ async function start() {
         .option('maxWorkers', {
           alias: ['mw'],
           default: 2,
-          demandOption: false,
+          demandOption: true,
           description:
             'The safer way to increase performance locally, sets the number of processes to use internally. Recommended 2',
           type: 'number',
